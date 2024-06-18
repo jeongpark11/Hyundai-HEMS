d0801d82ff408869a4f42aa65c5b0a548f4519a4
jeongpark11 <jeongpark@mwkorea.co.kr>
2024-06-18 17:44:30 +0900

    fix: 모달,관리자페이지,신규입력페이지

---
+++ dist/assets/js/ui-layout-script.js
@@ -0,0 +1,4 @@
+$(function(){
+    $('.header-place').load('./inc/header.html');
+    $('.modal-place').load('./inc/modals.html');
+});
--- dist/assets/js/ui-script.js
+++ dist/assets/js/ui-script.js
@@ -3,6 +3,7 @@ $(function(){
     // 카테고리 interaction
     const cat =  $('.categories .ct-title');
     const expCat = $('.categories .expand>ul li');
+    
     cat.click(function(){
         const exp =  $(this).parents('.expand');
         const ico =  $(this).children('i.open');
@@ -13,6 +14,7 @@ $(function(){
         exp.toggleClass('open'):
         (cat.parents('.expand').removeClass('open'),exp.toggleClass('open'));
     });
+
     expCat.click(function(){
         expCat.removeClass('active');
         $(this).addClass('active');
@@ -25,5 +27,41 @@ $(function(){
     page.click(function(){
         page.removeClass('active');
         $(this).addClass('active');
+    });
+
+    // 모달 interaction
+    const btnCk = $('.pop-ck');
+    const btnCl = $('.pop-cl');
+    const btnDel = $('.pop-del');
+    const tbBtnDel = $('button.del');
+    const btnChange = $('');
+
+    // 모달 보여지기/사라지기 함수 (모달dom, 상태(1:show, 0:hide))
+    const modalShowHide=function(modal,sh){
+        !sh? 
+        (modal.parents('.modal').removeClass('active'),
+        $('.dimmed').css('display','none'))
+        : 
+        (modal.addClass('active'),
+        $('.dimmed').css('display','block'));
+    };
+
+    btnCk.click(function(){
+        modalShowHide($(this),0);
     })
+
+    tbBtnDel.click(function(){
+        modalShowHide($('.modal.del'),1);
+    })
+    
+    btnCl.click(function(){
+        modalShowHide($(this),0);
+    })
+
+    btnDel.click(function(){
+        modalShowHide($(this),0);
+        modalShowHide($('.modal.fail'),1);
+    });
+
+    
 })
